483f11453698ed44f263b8218fdf7d503a92437a
Emily Smokoff <esmokoff@uw.edu>
2022-11-23 10:34:53 -0800

    Add link to browse products page

--- src/components/BookmarkedPage.js
+++ src/components/BookmarkedPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import { Link } from 'react-router-dom';
 import { Results } from './Results';
 
 export function BookmarkedPage(props) {
     return (
         <div>
             <div className='search-header'>
-                <div className='search-h2'>Your Bookmarked Products <span className="add">+ Add Items</span><span className="plus-icon">+</span></div>
+                <div className='search-h2'>Your Bookmarked Products <Link className="add" to="/BrowsePage">+ Add Items</Link><span className="plus-icon">+</span></div>
             </div>
             <div className='bookmarked'>
                 <Results
