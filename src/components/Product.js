7a96def0cf4b27152f488a679a0e8374a9ae3afc
“Akshita <akshu10@uw.edu>
2022-12-09 13:39:11 -0800

    Map

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import currentProductData from '../data/productData.json'
+import {
+    ComposableMap,
+    Geographies,
+    Geography,
+    Marker,
+    Annotation,
+    ZoomableGroup
+} from 'react-simple-maps';
+
+import ReactToolTip from "react-tooltip";
+
+import { MapContainer } from 'https://cdn.esm.sh/react-leaflet/MapContainer'
+import { TileLayer } from 'https://cdn.esm.sh/react-leaflet/TileLayer'
+import { useMap } from 'https://cdn.esm.sh/react-leaflet/hooks'
+
+import {
+    MapContainer,
+    TileLayer,
+    useMap,
+  } from 'https://cdn.esm.sh/react-leaflet'
 
 export function ProductPage(props) {
     return (
@@ -38,13 +58,30 @@ function Product(props) {
     )
 }
 
+
 function Map(props) {
+    const geoUrl = "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
+
+    const position = [51.505, -0.09]
     return (
-        <div className="map-card">
-                <h3>Availability near me</h3>
-                <p><button>Show availibility on maps</button></p>
-                <img className="map-img" src={"../img/map.jpeg"} alt={currentProductData.imageAlt} />
+        <div id="mapcontainer">
+            <p>Product Availability:</p>
+            <div id='mapid'>
+                <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={position}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
         </div>
     )
 }
+
+
 
